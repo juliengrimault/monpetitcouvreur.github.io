a9836d73019673d4528012cfcd61c55a407a0142
Julien Grimault <grimault.julien@gmail.com>
2023-03-03 17:20:26 -0800

    Add business card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,102 +1,36 @@
 import * as React from "react"
+import type * as CSS from 'csstype';
 import type { HeadFC, PageProps } from "gatsby"
+import BusinessCardImage from "./../images/BusinessCard.svg"
 
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 16,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
+  marginBottom: 30,
 }
 const headingAccentStyles = {
   color: "#663399",
 }
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+const imageStyle: CSS.Properties = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '75%',
+  objectFit: 'contain',
 }
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Mon Petit Couvreur ⛏️🏠
-        <br />
-        <span style={headingAccentStyles}>— Under Construction! 🚧👷🏻‍♂️🏗️</span>
+        Mon Petit Couvreur ⛏️🏠 <span style={headingAccentStyles}>— Under Construction! 🚧👷🏻‍♂️🏗️</span>
       </h1>
+      <img style={imageStyle} src={BusinessCardImage} alt="Business card" />
     </main>
   )
 }
