53cdd6cb830fd4a23db4050b91c0b14ad0327faa
Julien Grimault <julien_grimault@apple.com>
2024-07-04 20:39:45 -0700

    incorportate new design with old page still here
    add a hidden link in the existing page to link to the new page

---
+++ src/pages/index-new.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Layout from '../components/layout'
+import Slide_v1 from '../images/Slide_v1.jpg';
+import WhatWeDoSection from '../components/what-we-do-section'
+import WhyChooseUsSection from '../components/why-choose-us-section'
+import TestimonySection from "../components/testimony-section";
+
+const NewIndexPage = () => {
+  return (
+    <Layout>
+      <HeroSection />
+      <WhatWeDoSection />
+      <WhyChooseUsSection />
+      <TestimonySection />
+    </Layout>
+  )
+}
+
+const HeroSection = () => {
+  return (
+    <section className="bg-cover bg-top text-gray-100" style={{ backgroundImage: `url(${Slide_v1})` }}>
+    <div className="container mx-auto">
+      <div className="px-5 flex flex-col justify-center min-h-[45svh] sm:max-w-xl">
+        <div className="flex flex-col gap-5">
+          <h2 className="text-4xl font-extrabold">We Build Roof Expert Engineers</h2>
+          <p>We believe in providing top quality workmanship and are so confident in our level of service that we back it up with a good quality</p>
+        </div>
+      </div>
+    </div>
+    </section>
+  )
+}
+
+export default NewIndexPage
+
+export const Head = () => <title>Mon Petit Couvreur</title>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,41 @@
-import React from "react"
-import Layout from '../components/layout'
-import Slide_v1 from '../images/Slide_v1.jpg';
-import WhatWeDoSection from '../components/what-we-do-section'
-import WhyChooseUsSection from '../components/why-choose-us-section'
-import TestimonySection from "../components/testimony-section";
+import * as React from "react"
+import type * as CSS from 'csstype';
+import { HeadFC, Link, PageProps } from "gatsby"
+import BusinessCardImage from "./../images/BusinessCard.svg"
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <HeroSection />
-      <WhatWeDoSection />
-      <WhyChooseUsSection />
-      <TestimonySection />
-    </Layout>
-  )
+const pageStyles = {
+  color: "#232129",
+  padding: 16,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 30,
+}
+const headingAccentStyles = {
+  color: "#663399",
 }
 
-const HeroSection = () => {
-  return (
-    <section className="bg-cover bg-top text-gray-100" style={{ backgroundImage: `url(${Slide_v1})` }}>
-    <div className="container mx-auto">
-      <div className="px-5 flex flex-col justify-center min-h-[45svh] sm:max-w-xl">
-        <div className="flex flex-col gap-5">
-          <h2 className="text-4xl font-extrabold">We Build Roof Expert Engineers</h2>
-          <p>We believe in providing top quality workmanship and are so confident in our level of service that we back it up with a good quality</p>
-        </div>
-      </div>
-    </div>
-    </section>
-  )
+const imageStyle: CSS.Properties = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '75%',
+  objectFit: 'contain',
 }
 
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>
+        Mon Petit Couvreur ⛏️🏠 <span style={headingAccentStyles}>— Under Construction! 🚧👷🏻‍♂️<Link to='/index-new'>🏗️</Link></span>
+      </h1>
+      <img style={imageStyle} src={BusinessCardImage} alt="Business card" />
+      
+    </main>
+  )
+}
 
 export default IndexPage
 
-export const Head = () => <title>Mon Petit Couvreur</title>
+export const Head: HeadFC = () => <title>Mon Petit Couvreur</title>
